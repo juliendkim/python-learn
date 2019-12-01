cf7a4921f7024e921f32f9f314b8b473c2f1ec09
Dee Kim <juliendkim@gmail.com>
2019-12-02 01:30:16 +0900

    add cors, mysql

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify, Response, json
+from flask import Flask
 import os
 import sqlite3
+import flask_cors
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
+flask_cors.CORS(app)
 app.config.from_pyfile(os.path.join(app.root_path, 'config.py'))
+mysql = MySQL()
+mysql.init_app(app)
 
 
 def connect_db():
@@ -16,7 +21,25 @@ def connect_db():
 
 
 @app.route('/')
-def show_all():
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/', methods=['POST'])
+def post_hello():
+    from flask import request, jsonify
+    print(request.json)
+    req = request.json
+    school = req["school"]
+    dept = req["department"]
+
+    resdata = {'s': school, 'd': dept, 'id': 1, 'name': 'asdf'}
+    return jsonify(resdata)
+
+
+@app.route('/view')
+def get_sqlite3():
+    from flask import Response, json
     con, cur = connect_db()
     cur.execute('select id, title, content from entry')
     rows = cur.fetchall()
@@ -28,10 +51,25 @@ def show_all():
 
 @app.route('/add')
 def add():
+    from flask import jsonify
     con, cur = connect_db()
     cur.execute(app.config['QUERY_INSERT'])
     con.commit()
     return jsonify({'code': 0, 'result': 'added'})
+
+
+@app.route('/mysql')
+def get_mysql():
+    from flask import Response, json
+    query = 'select id, title, content from entry'
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    data = []
+    for row in rows:
+        data.append(dict(row))
+    return Response(json.dumps(data, ensure_ascii=False), content_type='application; charset=utf-8')
 
 
 if __name__ == '__main__':
